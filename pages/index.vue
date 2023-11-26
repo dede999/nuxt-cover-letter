ccc25ac12d9ce89eecb4c72c71e9105170742796
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2023-11-26 17:33:32 -0300

    Create upper elements in the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+  <div class="bg-gradient-to-r from-sky-500 to-emerald-500 px-8 py-4 m-4 rounded-lg">
+    <h1 class="text-4xl font-semibold"> Build Your Cover Letter </h1>
+  </div>
+  <div class="bg-emerald-200 container my-2 mx-auto rounded-lg">
+    <p class="p-5 text-xl text-center">
+      This is a tool I've built in order to exercise myself, building a cover letter
+      with Nuxt JS. The original idea has been built by Oluwafemi Akinyemi on an article
+      <a href="https://semaphoreci.com/blog/cover-letter-app-nextjs-chatgpt">
+        <b>in this link</b>.
+      </a>
+    </p>
+    <p class="p-5 text-xl text-center">
+      I do not intend to monetize from it, as it wasn't my idea, but someone else's. The reason
+      I'm asking for an identification is that I'm using a Chat GPT free tier, and I don't want
+      to have problems with the API limit
+    </p>
   </div>
 </template>
